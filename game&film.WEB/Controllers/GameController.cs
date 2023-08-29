33f404d3e2c5f16fae8c7c38e7cfcdf4032eb7e0
Hamza Yıldız <hamzayildiz2ekim@gmail.com>
2023-08-29 17:10:50 +0300

    .

---
+++ game&film.WEB/Controllers/GameController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace game_film.WEB.Controllers
+{
+    public class GameController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
