57fd9468e3bcc579bf39fa86125ab1bb338ce81a
Hamza Yıldız <hamzayildiz2ekim@gmail.com>
2023-08-29 11:13:53 +0300

    web develop

---
+++ game&film.WEB/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace game_film.WEB.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
